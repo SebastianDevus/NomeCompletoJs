f68a40c0644bba590ec67d1b049bf34597c0127a
Sebastian-Devus <86898507+SebastianDevus@users.noreply.github.com>
2022-11-10 22:11:52 -0300

    Adiciona vírgula que faltou

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ let nome = prompt("Digite seu primeiro nome: ");
 let sobrenome = prompt("Digite seu sobrenome: ");
 
 let nomeCompleto = nome + sobrenome;
-alert(`Seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catálogo é: ${sobrenome.toUpperCase()} ${nome}`);
+alert(`Seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catálogo é: ${sobrenome.toUpperCase()}, ${nome}`);
